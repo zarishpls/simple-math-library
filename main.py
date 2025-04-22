0fb899d33faf1b1f2d00df980f24ac17502929f5
fakhirsh <fakhir.shaheen@gmail.com>
2025-04-22 10:26:10 +0500

    div feature

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from my_math_lib import *
 
 print("2+2 = ", add(2,2))
 print("3*4 = ", mul(3,4))
+print("6/2 = ", div(6,2))
--- my_math_lib.py
+++ my_math_lib.py
@@ -8,3 +8,8 @@ def subtract(a, b):
     
 def mul(a,b):
     return a*b
+    
+def div(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
