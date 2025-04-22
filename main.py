a1fd094b03aac11511d2dd05264c86e07da501cf
fakhirsh <fakhir.shaheen@gmail.com>
2025-04-22 10:04:40 +0500

    my first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+from my_math_lib import *
+
+print("2+2 = ", add(2,2))
---
+++ my_math_lib.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    """Add two numbers and return the result."""
+    return a + b
+
+def subtract(a, b):
+    """Subtract b from a and return the result."""
+    return a - b
